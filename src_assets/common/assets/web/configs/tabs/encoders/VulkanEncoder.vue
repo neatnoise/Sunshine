dd08f201a9d1e8154a783a8590a5a891cd7a356c
neatnoise <neatnoise@users.noreply.github.com>
2026-01-19 03:05:07 +0100

    Add Vulkan encoder to web UI, restore glFinish sync

---
+++ src_assets/common/assets/web/configs/tabs/encoders/VulkanEncoder.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps([
+  'platform',
+  'config',
+])
+
+const config = ref(props.config)
+</script>
+
+<template>
+  <div id="vulkan-encoder" class="config-page">
+    <p>FFmpeg Vulkan Video encoder uses GPU hardware encoding via Vulkan API.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
